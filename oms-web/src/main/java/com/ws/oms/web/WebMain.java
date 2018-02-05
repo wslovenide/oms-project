01b79a0e6d8315d511e9e40273aeb69813c3f131
汪胜 <sheng.wang@chinaredstar.com>
2018-02-05 18:02:41 +0800

    s

--- oms-web/src/main/java/com/ws/oms/web/WebMain.java
+++ oms-web/src/main/java/com/ws/oms/web/WebMain.java
@@ -1,7 +1,5 @@
 package com.ws.oms.web;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -16,10 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebMain {
 
-    private static Logger logger = LoggerFactory.getLogger(WebMain.class);
-
     public static void main(String[] args) {
-        logger.info("开始启动web应用......");
         SpringApplication.run(WebMain.class,args);
     }
 
