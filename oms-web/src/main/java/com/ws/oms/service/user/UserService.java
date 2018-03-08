5cbd460b17bef503bad62629966566294c9931a9
汪胜 <sheng.wang@chinaredstar.com>
2018-03-08 11:16:08 +0800

    s

---
+++ oms-web/src/main/java/com/ws/oms/service/user/UserService.java
@@ -0,0 +1,14 @@
+package com.ws.oms.service.user;
+
+import com.ws.oms.service.user.api.IUserService;
+
+/**
+ * Description:
+ *
+ * @author sheng.wang
+ * @version 1.0.0
+ * @email sheng.wang@chinaredstar.com
+ * @date: 2018-03-08 11:15
+ */
+public class UserService implements IUserService {
+}
---
+++ oms-web/src/main/java/com/ws/oms/service/user/api/IUserService.java
@@ -0,0 +1,12 @@
+package com.ws.oms.service.user.api;
+
+/**
+ * Description:
+ *
+ * @author sheng.wang
+ * @version 1.0.0
+ * @email sheng.wang@chinaredstar.com
+ * @date: 2018-03-08 11:15
+ */
+public interface IUserService {
+}
