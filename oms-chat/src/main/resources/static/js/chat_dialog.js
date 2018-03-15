aa6da28bebc111a218572491e93e17c9faf0c5b6
汪胜 <sheng.wang@chinaredstar.com>
2018-03-15 16:42:57 +0800

    s

--- oms-chat/src/main/resources/static/js/chat_dialog.js
+++ oms-chat/src/main/resources/static/js/chat_dialog.js
@@ -1,4 +1,3 @@
-
 var httpRequestUrl = "http://" + serverUrl;
 function createChatRoom(ele) {
     var toSessionId = $(ele).attr("sessionId");
@@ -9,20 +8,25 @@ function createChatRoom(ele) {
             alert(resp.msg);
             return;
         }
-        layer.ready(function(){
-            layer.open({
-                id:resp.msg,
-                type: 2,
-                title: $(ele).text(),
-                maxmin: true,
-                shade: false,
-                area: ['600px', '400px'],
-                content: ['../static/chat_window.html','no'],
-                end: function(){
-                    // 关闭时会触发
-                    // layer.tips('Hi', '#about', {tips: 1});
-                }
-            });
+        layer.open({
+            id:resp.msg,
+            type: 2,
+            title: $(ele).text(),
+            maxmin: true,
+            shade: false,
+            area: ['600px', '400px'],
+            zIndex: layer.zIndex,
+            content: ['../static/chat_window.html','no'],
+            success:function (layero, index) {
+                // 打开窗口后回调
+                // console.info(ws); 查询聊天记录
+                // layer.setTop(layero); // 再次点击时置顶
+            },
+            end: function(){
+                // 关闭时会触发
+                // layer.tips('Hi', '#about', {tips: 1});
+                layerIndex = null;
+            }
         });
     });
 }
