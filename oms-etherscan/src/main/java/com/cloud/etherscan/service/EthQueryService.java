bf38dd67832d3758fd9cea19e6f03a9a9948bb0d
汪胜 <403059609@qq.com>
2018-05-22 20:32:36 +0800

    s

--- oms-etherscan/src/main/java/com/cloud/etherscan/service/EthQueryService.java
+++ oms-etherscan/src/main/java/com/cloud/etherscan/service/EthQueryService.java
@@ -6,16 +6,18 @@ import com.cloud.etherscan.model.EthHolderDetail;
 import com.cloud.etherscan.tool.HtmlParserUtil;
 import com.cloud.etherscan.tool.URLEncoderUtil;
 import com.cloud.etherscan.tool.http.HttpTookit;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -42,6 +44,9 @@ public class EthQueryService {
 
     @Value("${eth.token.searchHandler.host}")
     private String ethSearchHandlerHost;
+
+    @Value("${eth.token.save.path}")
+    private String savePath;
 
     public void queryTokenByName(List<String> names){
         for (String name : names){
@@ -99,12 +104,14 @@ public class EthQueryService {
         // 查询总数
         String tokenCount = getTotalCount(token);
 
-        calculateStatistic(all,tokenCount);
+        calculateStatistic(all,tokenCount,ethName);
+
+        System.out.println("生成 [" + ethName +"] 完成");
 
     }
 
 
-    public void calculateStatistic(List<EthHolderDetail> list,String totalCount){
+    public void calculateStatistic(List<EthHolderDetail> list,String totalCount,String ethName){
         BigDecimal top10  = BigDecimal.ZERO , top20 = BigDecimal.ZERO, top50 = BigDecimal.ZERO;
         BigDecimal top100 = BigDecimal.ZERO, top200 = BigDecimal.ZERO, top500 = BigDecimal.ZERO;
 
@@ -129,13 +136,89 @@ public class EthQueryService {
             }
         }
         BigDecimal totalToken = new BigDecimal(totalCount);
-        top10.divide(totalToken,4,BigDecimal.ROUND_HALF_UP);
-        top20.divide(totalToken,4,BigDecimal.ROUND_HALF_UP);
-        top50.divide(totalToken,4,BigDecimal.ROUND_HALF_UP);
-        top100.divide(totalToken,4,BigDecimal.ROUND_HALF_UP);
-        top200.divide(totalToken,4,BigDecimal.ROUND_HALF_UP);
-        top500.divide(totalToken,4,BigDecimal.ROUND_HALF_UP);
+        BigDecimal multi = new BigDecimal(100);
+        BigDecimal divide = top10.multiply(multi).divide(totalToken, 2, BigDecimal.ROUND_HALF_UP);
+        BigDecimal divide1 = top20.multiply(multi).divide(totalToken, 2, BigDecimal.ROUND_HALF_UP);
+        BigDecimal divide2 = top50.multiply(multi).divide(totalToken, 2, BigDecimal.ROUND_HALF_UP);
+        BigDecimal divide3 = top100.multiply(multi).divide(totalToken, 2, BigDecimal.ROUND_HALF_UP);
+        BigDecimal divide4 = top200.multiply(multi).divide(totalToken, 2, BigDecimal.ROUND_HALF_UP);
+        BigDecimal divide5 = top500.multiply(multi).divide(totalToken, 2, BigDecimal.ROUND_HALF_UP);
 
+        try {
+            String date = new SimpleDateFormat("MM月dd日").format(new Date());
+
+            HSSFWorkbook workbook = new HSSFWorkbook();
+            HSSFSheet sheet = workbook.createSheet(date);
+
+            HSSFRow row = sheet.createRow(0);
+            row.createCell(1).setCellValue(ethName+"持有数据汇总("+date+")");
+            row.createCell(2).setCellValue("数量");
+            row.createCell(3).setCellValue("占比");
+
+
+            HSSFRow row1 = sheet.createRow(1);
+            row1.createCell(1).setCellValue("发行总量");
+            row1.createCell(2).setCellValue(totalCount);
+            row1.createCell(3).setCellValue("100%");
+
+            HSSFRow row2 = sheet.createRow(2);
+            row2.createCell(1).setCellValue("TOP10持有量");
+            row2.createCell(2).setCellValue(top10.toString());
+            row2.createCell(3).setCellValue(divide.toString()+"%");
+
+
+            HSSFRow row3 = sheet.createRow(3);
+            row3.createCell(1).setCellValue("TOP20持有量");
+            row3.createCell(2).setCellValue(top20.toString());
+            row3.createCell(3).setCellValue(divide1.toString()+"%");
+
+
+            HSSFRow row4 = sheet.createRow(4);
+            row4.createCell(1).setCellValue("TOP50持有量");
+            row4.createCell(2).setCellValue(top50.toString());
+            row4.createCell(3).setCellValue(divide2.toString()+"%");
+
+
+            HSSFRow row5 = sheet.createRow(5);
+            row5.createCell(1).setCellValue("TOP100持有量");
+            row5.createCell(2).setCellValue(top100.toString());
+            row5.createCell(3).setCellValue(divide3.toString()+"%");
+
+            HSSFRow row6 = sheet.createRow(6);
+            row6.createCell(1).setCellValue("TOP200持有量");
+            row6.createCell(2).setCellValue(top200.toString());
+            row6.createCell(3).setCellValue(divide4.toString()+"%");
+
+            HSSFRow row7 = sheet.createRow(7);
+            row7.createCell(1).setCellValue("TOP500持有量");
+            row7.createCell(2).setCellValue(top500.toString());
+            row7.createCell(3).setCellValue(divide5.toString()+"%");
+
+
+            HSSFRow row9 = sheet.createRow(9);
+            row9.createCell(0).setCellValue("排名");
+            row9.createCell(1).setCellValue("持有者地址");
+            row9.createCell(2).setCellValue("数量");
+            row9.createCell(3).setCellValue("百分比");
+
+            for (int i = 0 ; i < list.size(); i++){
+                HSSFRow row10 = sheet.createRow(10+i);
+
+                EthHolderDetail detail = list.get(i);
+
+                row10.createCell(0).setCellValue(i+1);
+                row10.createCell(1).setCellValue(detail.getAddress());
+                row10.createCell(2).setCellValue(detail.getQuantity());
+                row10.createCell(3).setCellValue(detail.getPercentage());
+
+            }
+            File file = new File(new File(savePath),ethName+date+".xlsx");
+
+            workbook.write(file);
+            workbook.close();
+        }catch (Exception e){
+            logger.error("生成文件出错!",e);
+        }
     }
 
 
