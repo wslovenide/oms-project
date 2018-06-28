594f86f1af45426590196c00cf75d47c3ea75a6e
汪胜 <403059609@qq.com>
2018-06-28 22:35:09 +0800

    s

--- oms-service/src/main/java/com/ws/service/process/MatchService.java
+++ oms-service/src/main/java/com/ws/service/process/MatchService.java
@@ -1,9 +1,9 @@
 package com.ws.service.process;
 
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import org.springframework.util.CollectionUtils;
+
+import java.util.*;
 
 /**
  * Created by gongmei on 2018/6/28.
@@ -12,18 +12,18 @@ public class MatchService {
 
     private Map<String,Object> map = new HashMap<>(1024);
 
+    private Map<String,List<String>> listMap = new HashMap<>(64);
+
 
     public void match(String fileName){
         List<String> list = Constants.ITEM_MAP.get(fileName);
-
-        for (String item : list){
-
-
-
-
+        if(CollectionUtils.isEmpty(list)){
+            return;
         }
+        String[] split = fileName.split(Constants.ITEM_SEPARATOR);
+        listMap.computeIfAbsent(split[0], key -> new ArrayList<>()).add(split[1]);
 
-
+        
 
 
 
